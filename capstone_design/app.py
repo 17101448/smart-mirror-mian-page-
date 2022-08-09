39cb3b5c755cafc835d58208b13beb9b3b4dc3e3
17101448 <the_1_week@naver.com>
2022-08-09 21:35:06 +0900

    날씨에 따라 변하는 아이콘과 날씨 정보

--- capstone_design/app.py
+++ capstone_design/app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 #__name__ 인자는 정적파일과 템플릿을 찾는 데 쓰임 
 import crawling
-
+import weather 
 
 @app.route('/')
 def hello():
     news_list, href, img = crawling.news_crawling()
     img_list = crawling.img_src(news_list)
     sum_list = crawling.summary(news_list)
-    return render_template('index.html', news_list = news_list, href = href, img = img, img_list=img_list, len = len(news_list), sum = sum_list )
+    weather_info = weather.return_weather_info() 
+    return render_template('index.html', news_list = news_list, href = href, img = img, img_list=img_list, len = len(news_list), sum = sum_list, weather_info=weather_info )
 
 if __name__ == "__main__": 
 	app.run(debug=True) 
